82509f93fb6925ecc474c8622f84f5f8c462f66d
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-17 19:07:20 +0200

    algo falla

--- ejercicio3.py
+++ ejercicio3.py
@@ -6,4 +6,26 @@ class Naturaleza:
 class Producto(Naturaleza):
     def __init__(self,iva):
         self.iva = iva
-        self.producto = 100
+        self.iva = 100
+    def facturar(self):
+        return 100 +self.iva
+
+class Factory(Producto):
+    def __init__(self):
+        pass
+    def crear(self):
+        clase = Producto(self.iva)
+        return clase
+    
+#Declaramos Naturaleza como instancia de la clase Naturaleza, para poder aplicar el iva correspondiente
+Naturaleza = Naturaleza()
+
+producto = Producto(Naturaleza.ALIMENTARIA)
+precio_con_impuestos = Factory.crear(producto).facturar()
+print(precio_con_impuestos)
+# 105.5
+
+producto = Producto(Naturaleza.SERVICIO)
+precio_con_impuestos = Factory.crear(producto).facturar()
+print(precio_con_impuestos)
+# 120
