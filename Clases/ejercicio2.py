f098ec58b2c42785317050fd98aa8d6543a28553
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-17 20:24:55 +0200

    upper y entrada

--- Clases/ejercicio2.py
+++ Clases/ejercicio2.py
@@ -1,5 +1,14 @@
-class entrada:
+class Entrada:
     def __init__(self,linea1,linea2):
         self.linea1 = linea1
         self.linea2 = linea2
-        
+class Mayuscula():
+    def __init__(self,datos):
+        self.datos = Entrada
+        self.datos.linea1 = datos.linea1.upper()
+        self.datos.linea2 = datos.linea2.upper()
+
+       
+linea1 = input("Introduzca la primera linea del archivo:")
+linea2 = input("Introduce la segunda linea del archivo:")
+datos = Entrada(linea1,linea2)
